19a54f6beb02f8d4da71c316470c4b5480f5028d
Jiri Klein <jiklein@deloitte.co.uk>
2018-04-26 16:15:22 +0100

    merge conflict resolved

--- HelloWorld_GA.py
+++ HelloWorld_GA.py
@@ -94,7 +94,7 @@ while (max((o.fitness) for o in entities) != 1):
 	entities.sort(key = lambda x: x.fitness, reverse = True)
 
 	# kill worst half
-	kill = 250
+	kill = init_population / 5
 	entities = entities[0:kill]
 
 	# generate again
